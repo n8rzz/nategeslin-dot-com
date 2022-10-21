13a6d10a187c7815619a3f0beaeca5e041ea2482
nate geslin <teamtomkins23@gmail.com>
2022-10-21 17:47:52 -0500

    adds subHading to front_matter and PostLayout files

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -17,7 +17,7 @@ const discussUrl = (slug) =>
 const postDateTemplate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
-  const { slug, fileName, date, title, images, tags } = frontMatter
+  const { slug, fileName, date, title, images, subHeading, tags } = frontMatter
 
   return (
     <SectionContainer>
@@ -43,6 +43,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
               </dl>
               <div>
                 <PageTitle>{title}</PageTitle>
+                {subHeading !== '' && <h2>{subHeading}</h2>}
               </div>
             </div>
           </header>
