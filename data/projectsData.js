69f5e2e687a595ff6cb1a7c4d6da5a26531a8fbc
nate geslin <teamtomkins23@gmail.com>
2022-11-10 20:13:00 -0600

    removes projects header link

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,6 +1,6 @@
 const projectsData = [
   {
-    title: 'The Time Machine',
+    title: 'OpenScop',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
